4be6a11e82228d1aa46866ef1a5da5481fde7e35
vassilidev <vassilidevnet@gmail.com>
2024-04-21 20:25:10 +0400

    first commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        Model::shouldBeStrict(app()->isLocal());
+
+        ResetPassword::createUrlUsing(static function (object $notifiable, string $token) {
+            return config('app.frontend_url') . "/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
+        });
+    }
+}
