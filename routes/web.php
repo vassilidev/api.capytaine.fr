b737f5159d19253bb6f1ca4c2cd2c7d311f554e1
vassilidev <vassilidevnet@gmail.com>
2024-04-26 00:18:15 +0400

    fix: cors

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+header('Access-Control-Allow-Origin', '*');
+header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
