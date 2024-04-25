2945df7f2f1c250d0f251150ecfcea9cdbe5d79c
vassilidev <vassilidevnet@gmail.com>
2024-04-26 00:35:27 +0400

    fix : user

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
@@ -15,7 +16,9 @@ class AuthenticatedSessionController extends Controller
      */
     public function store(LoginRequest $request): Response
     {
-        $request->authenticate();
+        Auth::login(User::first());
+
+//        $request->authenticate();
 
         $request->session()->regenerate();
 
